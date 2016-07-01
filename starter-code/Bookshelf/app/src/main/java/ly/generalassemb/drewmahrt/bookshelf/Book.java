71708b5806e5609830935ddfcb05d287d967d8f5
Freatnor <jonathan.randall.taylor@gmail.com>
2016-07-01 16:12:27 -0400

    Adding completed Lab w/o bonus

--- starter-code/Bookshelf/app/src/main/java/ly/generalassemb/drewmahrt/bookshelf/Book.java
+++ starter-code/Bookshelf/app/src/main/java/ly/generalassemb/drewmahrt/bookshelf/Book.java
@@ -6,10 +6,14 @@ package ly.generalassemb.drewmahrt.bookshelf;
 public class Book {
     private String mTitle;
     private String mAuthor;
+    private int mYearPublished;
+    private int mPages;
 
-    public Book(String title,String author){
+    public Book(String title,String author, int yearPublished, int pages){
         mTitle = title;
         mAuthor = author;
+        mYearPublished = yearPublished;
+        mPages = pages;
     }
 
     public String getTitle() {
@@ -20,4 +24,11 @@ public class Book {
         return mAuthor;
     }
 
+    public int getYearPublished() {
+        return mYearPublished;
+    }
+
+    public int getPages() {
+        return mPages;
+    }
 }
--- starter-code/Bookshelf/app/src/main/java/ly/generalassemb/drewmahrt/bookshelf/MainActivity.java
+++ starter-code/Bookshelf/app/src/main/java/ly/generalassemb/drewmahrt/bookshelf/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -20,8 +21,11 @@ public class MainActivity extends AppCompatActivity {
     BaseAdapter mBookAdapter;
 
     //TODO: Define your ListView
+    ListView mListView;
 
     //TODO: Define your Book List
+    List<Book> mBookList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Use helper method to add books to the list
         mBookList = generateBooks();
+        mListView = (ListView) findViewById(R.id.listview);
 
         //TODO: Instantiate BaseAdapter
         //Below is a partially complete example for one Adapter
@@ -52,10 +57,36 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
                 //TODO: Update the view
+                View view = convertView;
+                if(view == null){
+                    LayoutInflater inflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    view = inflater.inflate(android.R.layout.simple_list_item_2, null);
+                }
+
+                TextView bookNameView = (TextView) view.findViewById(android.R.id.text1);
+                TextView authorNameView = (TextView) view.findViewById(android.R.id.text2);
+
+                bookNameView.setText(mBookList.get(position).getTitle());
+                authorNameView.setText(mBookList.get(position).getAuthor());
+
+                return view;
             }
         };
 
         //TODO: Set the ListView's adapter
+        mListView.setAdapter(mBookAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView bookNameView = (TextView) view.findViewById(android.R.id.text1);
+                TextView authorNameView = (TextView) view.findViewById(android.R.id.text2);
+
+
+                bookNameView.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+                authorNameView.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
+            }
+        });
 
     }
 
@@ -63,12 +94,12 @@ public class MainActivity extends AppCompatActivity {
     private List<Book> generateBooks(){
         List<Book> books = new ArrayList<>();
 
-        books.add(new Book("Apples Book","Brad"));
-        books.add(new Book("Cats Book","Ryan"));
-        books.add(new Book("Eagles Book","Kate"));
-        books.add(new Book("Strawberries Cathy","Brad"));
-        books.add(new Book("Dogs Book","Tom"));
-        books.add(new Book("Ants Book","Zane"));
+        books.add(new Book("Apples Book","Brad",2002,30));
+        books.add(new Book("Cats Book","Ryan",25,400));
+        books.add(new Book("Eagles Book","Kate",-70,2));
+        books.add(new Book("Strawberries Cathy","Brad",1876,500));
+        books.add(new Book("Dogs Book","Tom",1297,7000));
+        books.add(new Book("Ants Book","Zane",2025,20205));
 
         return books;
     }
